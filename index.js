0be3c7940215f1df83868a0e3de28d325f0186e1
skpayaya <sashi.kumar.patelskp@gmail.com>
2022-01-24 16:30:34 +0530

    refactoring into separete modules

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -14,7 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
@@ -34,7 +35,7 @@ app.post("/api/blogs", (request, response) => {
     });
 });
 
-const PORT = process.env.PORT;
+const PORT = config.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    logger.info(`Server running on port ${PORT}`);
 });
